8aa87bf99dcc453feadca264560dddbe37ce1fad
Amir partovi <a.partovi99@gmail.com>
2022-11-24 11:57:01 +0330

    movieType

--- Components/Layout/Layout.tsx
+++ Components/Layout/Layout.tsx
@@ -1,46 +1,69 @@
-import Grid from '@mui/material/Grid/Grid'
-import Typography from '@mui/material/Typography/Typography'
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import Avatar from "@mui/material/Avatar";
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+
 import Image from 'next/image'
-import React, { ReactNode } from 'react'
-import Logo from '../../Assets/Images/fa-filimo-dark-logo.png'
-import Divider from '@mui/material/Divider';
-import PersonIcon from '@mui/icons-material/Person';
-import Avatar from '@mui/material/Avatar/Avatar'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-type Props = {
-    children:ReactNode
+
+import {ReactNode, useState} from "react";
+
+import Logo from '../../Assets/images/fa-filimo-dark-logo.png'
+import {movieTypes} from "../../Data/movieTypes";
+
+interface LayoutProps {
+    children: ReactNode
 }
 
-const Layout = ({children}:Props) => {
-  return (
-    <Grid>
-        <Grid container width={'100%'} height={50} bgcolor={'common.black'} direction={'row'}>
-            <Grid display={'flex'} item xs={8}  alignItems={'center'}>
-                <Grid justifyContent={'center'} xs={1.5}>
-                    <Image src={Logo} height={25} alt={'filimo'} />
+const Layout = ({children}: LayoutProps) => {
+    const [display, setDisplay] = useState<boolean>(false)
+    return (
+        <Grid>
+            <Grid container width={'100%'} height={50} sx={{background: 'rgba(0,0,0,.8)'}}>
+                <Grid container item xs={8} alignItems={'center'}>
+                    <Grid display={'flex'} item xs={1.5} justifyContent={'center'}>
+                        <Image src={Logo} alt={'logo'} width={80} height={25}/>
+                    </Grid>
+                    <Grid display={'flex'} item xs={1} justifyContent={'center'} height={'50%'} alignItems={'center'}>
+                        <Divider sx={{backgroundColor: '#E0E0E0'}} orientation="vertical" variant="middle"/>
+                    </Grid>
+                    <Grid display={'flex'} item alignItems={'center'} height={'100%'} position={'relative'}
+                          onMouseEnter={() => setDisplay(true)} onMouseLeave={() => setDisplay(false)}>
+                        <Grid display={'flex'}
+                              sx={{cursor: 'pointer', color: 'common.white', '&:hover': {color: '#f9ad03'}}}>
+                            <Typography>
+                                فیلم
+                            </Typography>
+                            <KeyboardArrowDownRoundedIcon/>
+                        </Grid>
+                        <Grid width={250} height={"auto"} bgcolor={'common.black'} display={display ? 'flex' : 'none'}
+                              position={'absolute'} top={40} right={0} borderRadius={2} color={'common.white'}
+                              onMouseEnter={() => setDisplay(true)} onMouseLeave={() => setDisplay(false)}>
+                            <Grid container p={1}>
+                                {movieTypes.map( m => (
+                                    <Grid item xs={6} key={m.id}>
+                                        <Typography sx={{cursor: 'pointer', color: 'common.white', '&:hover': {color: '#f9ad03'}}}>
+                                            {m.title}
+                                        </Typography>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
-                <Grid display={'flex'} item xs={1} justifyContent={'center'}  alignItems={'center'}>
-                <Divider orientation="vertical" variant="middle" flexItem sx={{color:'#fff'}}/>
-                </Grid>
-                <Grid display={'flex'} sx={{cursor:'pointer',color:'common.white'}}>
-                    <Typography color={'common.white'}>
-                      {'فیلم'}
-                    </Typography>
-                    <KeyboardArrowDownIcon sx={{color:'#fff'}}/>
+                <Grid display={'flex'} item xs={4} justifyContent={'flex-end'}>
+                    <Grid display={'flex'} alignItems={'center'} ml={2} sx={{cursor: 'pointer'}}>
+                        <Avatar sx={{width: 30, height: 30}}>
+                            <PersonRoundedIcon/>
+                        </Avatar>
+                        <KeyboardArrowDownRoundedIcon sx={{color: '#E0E0E0'}}/>
+                    </Grid>
                 </Grid>
             </Grid>
-            <Grid display={'flex'} item xs={4} justifyContent={'flex-end'}>
-            <Grid display={'flex'} alignItems={'center'} ml={2} sx={{cursor:'pointer'}}>
-            <Avatar >
-                <PersonIcon/>
-              </Avatar>
-              <KeyboardArrowDownIcon sx={{color:'#fff'}}/>
-            </Grid>
-            </Grid>
+            {children}
         </Grid>
-        {children}
-    </Grid>
-  )
+    )
 }
 
 export default Layout
---
+++ Data/movieTypes.tsx
@@ -0,0 +1,7 @@
+import {movieType} from "../Model/Model";
+
+export const movieTypes: movieType[] = [
+    {id:1,title:'انیمیشن'},
+    {id:2,title:'هیجان انگیز'},
+    {id:3,title:'ترسناک'},
+]
